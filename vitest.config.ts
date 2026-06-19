0c104ac808817894f9d92d159a274254e3ff12cb
Eryka Mileuskaya <rika.gumerova@gmail.com>
2026-06-20 01:05:30 +0300

    chore: 4 - update vitest config path

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,4 +1,8 @@
 import { defineConfig } from 'vitest/config';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
 export default defineConfig({
   test: {
@@ -26,7 +30,7 @@ export default defineConfig({
   },
   resolve: {
     alias: {
-      '@': new URL('src', import.meta.url).pathname,
+      '@': path.resolve(__dirname, './src'),
     },
   },
 });
