cd9f52a5b4996b766e8ebea418fb8d32530c52c1
Eryka Mileuskaya <rika.gumerova@gmail.com>
2026-06-20 00:10:56 +0300

    feat: 4 - add index styles

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from 'next';
 import type { ReactNode } from 'react';
 import { Inter, JetBrains_Mono } from 'next/font/google';
-import '../styles/globals.css';
+import '@/styles/index.css';
 
 const inter = Inter({
   variable: '--font-inter',
