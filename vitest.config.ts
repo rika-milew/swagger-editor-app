07e5399411e1b7a09be691661efd86409011bf7c
Eryka Mileuskaya <rika.gumerova@gmail.com>
2026-06-20 00:29:24 +0300

    feat: 4 - update test configs

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,22 +1,17 @@
 import { defineConfig } from 'vitest/config';
-import react from '@vitejs/plugin-react';
-import path from 'path';
 
 export default defineConfig({
-  plugins: [react()],
   test: {
     environment: 'jsdom',
     globals: true,
-    setupFiles: ['./src/tests/setup-tests.ts'],
+    setupFiles: ['./src/test-utils/setup-tests.ts'],
     include: ['src/**/*.{test,spec}.{ts,tsx}'],
     coverage: {
       provider: 'v8',
       reporter: ['text', 'json', 'html'],
       exclude: [
         'node_modules/',
-        'src/tests/',
-        'src/**/*.test.{js,jsx,ts,tsx}',
-        'src/**/*.spec.{js,jsx,ts,tsx}',
+        'src/**/*.{test,spec}.{ts,tsx}',
         '**/*.d.ts',
         '**/*.config.*',
         '**/layout.tsx',
