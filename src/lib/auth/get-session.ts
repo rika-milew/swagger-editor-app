58c7184f299d35c34cb3f7284b40b3e28a7682ee
Eryka Mileuskaya <rika.gumerova@gmail.com>
2026-06-24 22:55:54 +0300

    refactor: 32 - rename supabase folder to database

--- src/lib/auth/get-session.ts
+++ src/lib/auth/get-session.ts
@@ -1,4 +1,4 @@
-import { createClient } from '@/lib/supabase/server';
+import { createClient } from '@/lib/database/server';
 import type { User } from '@supabase/supabase-js';
 
 export async function getSession(): Promise<User | null> {
