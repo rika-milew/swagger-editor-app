6ea8a90d7d11b9efe62030f4cd753f968e40ae3e
Eryka Mileuskaya <rika.gumerova@gmail.com>
2026-06-24 23:42:57 +0300

    feat: 8 - add cva

--- src/components/button.tsx
+++ src/components/button.tsx
@@ -1,37 +1,55 @@
 import classNames from 'classnames/bind';
+import { cva, type VariantProps } from 'class-variance-authority';
 import type { MouseEventHandler, ReactNode } from 'react';
 
 import styles from './button.module.css';
 
 const cx = classNames.bind(styles);
 
-type ButtonColor = 'basic' | 'primary' | 'secondary' | 'transparent';
+const buttonVariants = cva(cx('button'), {
+  variants: {
+    color: {
+      basic: cx('basic'),
+      primary: cx('primary'),
+      secondary: cx('secondary'),
+      transparent: cx('transparent'),
+    },
+    size: {
+      small: cx('small'),
+      medium: cx('medium'),
+    },
+    shape: {
+      rounded: cx('rounded'),
+      square: cx('square'),
+    },
+    isActive: {
+      true: cx('active'),
+    },
+  },
+  defaultVariants: {
+    color: 'basic',
+    size: 'medium',
+    shape: 'square',
+  },
+});
 
-type ButtonSize = 'small' | 'medium';
-
-type ButtonShape = 'rounded' | 'square';
-
-type ButtonType = 'button' | 'submit';
+type ButtonVariants = VariantProps<typeof buttonVariants>;
 
 type ButtonProps = {
   children: ReactNode;
   className?: string;
-  type?: ButtonType;
-  color?: ButtonColor;
-  size?: ButtonSize;
-  shape?: ButtonShape;
+  type?: 'button' | 'submit';
   onClick?: MouseEventHandler<HTMLButtonElement>;
-  isActive?: boolean;
   disabled?: boolean;
-};
+} & ButtonVariants;
 
 export const Button = ({
   children,
   className,
-  type = 'button',
-  color = 'basic',
-  size = 'medium',
-  shape = 'square',
+  type,
+  color,
+  size,
+  shape,
   isActive = false,
   disabled = false,
   ...rest
@@ -39,14 +57,7 @@ export const Button = ({
   return (
     <button
       type={type}
-      className={cx(
-        'button',
-        color,
-        size,
-        shape,
-        { active: isActive },
-        className,
-      )}
+      className={buttonVariants({ color, size, shape, isActive, className })}
       disabled={disabled}
       {...rest}
     >
