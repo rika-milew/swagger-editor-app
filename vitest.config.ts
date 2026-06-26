eb8e149d1cb80386871653f1a4242a9bbdf22e32
Eryka Mileuskaya <rika.gumerova@gmail.com>
2026-06-27 01:41:00 +0300

    feat: 45 - add auth form test

--- vitest.config.ts
+++ vitest.config.ts
@@ -19,6 +19,7 @@ export default defineConfig({
         '**/*.d.ts',
         '**/*.config.*',
         '**/layout.tsx',
+        'src/theme/**',
       ],
       thresholds: {
         statements: 80,
