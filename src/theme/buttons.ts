0b867e5c60342fd0683b910cea43f014ac57e523
Eryka Mileuskaya <rika.gumerova@gmail.com>
2026-06-27 23:04:06 +0300

    refactor: 45 - style refactoring

--- src/theme/buttons.ts
+++ src/theme/buttons.ts
@@ -1,55 +1,6 @@
 import { colors } from './colors';
 
 export const buttons = {
-  signIn: {
-    variant: 'ghost',
-    bg: 'transparent',
-    color: colors.nav.inactive,
-    fontWeight: '500',
-    borderRadius: 'full',
-
-    _hover: {
-      bg: 'transparent',
-      color: colors.nav.active,
-    },
-  },
-
-  signUp: {
-    bg: colors.colorWhite,
-    color: colors.background,
-    fontWeight: '600',
-    borderRadius: 'full',
-    px: 5,
-
-    _hover: {
-      bg: colors.brandPrimary,
-    },
-  },
-  languageSwitcher: {
-    variant: 'ghost',
-    bg: 'transparent',
-    color: colors.colorZinc600,
-
-    _hover: {
-      bg: 'transparent',
-      color: colors.colorWhite,
-    },
-  },
-  auth: {
-    variant: 'ghost',
-    bg: 'transparent',
-    color: colors.colorAuth,
-    border: '1px solid',
-    borderColor: colors.border,
-    borderRadius: 'sm',
-    fontWeight: '500',
-    px: 4,
-
-    _hover: {
-      bg: 'transparent',
-      color: colors.brandPrimary,
-    },
-  },
   submit: {
     bg: colors.brandPrimary,
     color: colors.background,
