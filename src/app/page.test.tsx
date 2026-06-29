255f4f1bc58b657f4c0a384fcd98e0dc497533a8
Нина Евлаш <evlash.nina@mail.ru>
2026-06-29 23:23:54 +0200

    refactor: 7 - update typography and font system add chakra UI styles to the Home page add chakra Provider to the page test

--- src/app/page.test.tsx
+++ src/app/page.test.tsx
@@ -1,10 +1,15 @@
 import { render, screen } from '@testing-library/react';
 import { describe, it, expect } from 'vitest';
 import Home from './page';
+import { Provider } from '@/providers/chakraProvider';
 
 describe('Home page', () => {
   it('renders the main heading', () => {
-    render(<Home />);
+    render(
+      <Provider>
+        <Home />
+      </Provider>,
+    );
     expect(
       screen.getByRole('heading', { name: /swagger editor app/i }),
     ).toBeInTheDocument();
