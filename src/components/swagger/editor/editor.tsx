88dc6c2f1ae901a2d63caf8303d7ac87ca22e441
michaeltavyrin <105814138+bssier@users.noreply.github.com>
2026-06-30 19:38:51 +0300

    26-feat: Split View Layout (#56)
    * feat: 26 - split view layout
    
    * fix: 26 - problem with adaptive layout
    
    * fix: 26 - rename home to homeroute
    
    * fix: 26 - separate use client
    
    * fix: 26 - changes after review
    
    * fix: 26 - changes after review
    
    * fix: 26 - changes after review
    
    * fix: 26 - rename resent-performance
    
    * fix: 26 - move the code from the mine component to page and group styles by types and by ascending

---
+++ src/components/swagger/editor/editor.tsx
@@ -0,0 +1,24 @@
+'use client';
+
+import CodeMirror from '@uiw/react-codemirror';
+import { yaml } from '@codemirror/lang-yaml';
+import { oneDark } from '@codemirror/theme-one-dark';
+import { customTheme } from './editor-theme';
+
+export const Editor = () => {
+  return (
+    <CodeMirror
+      value="# Write code here!"
+      width="100%"
+      theme={oneDark}
+      style={{
+        flex: 1,
+        display: 'flex',
+        flexDirection: 'column',
+        width: '100%',
+        minHeight: 0,
+      }}
+      extensions={[yaml(), customTheme]}
+    />
+  );
+};
