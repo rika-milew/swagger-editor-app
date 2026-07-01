c78cfeadf48d6173450827466920722ef5c54f8c
Eryka Mileuskaya <rika.gumerova@gmail.com>
2026-07-01 23:42:21 +0300

    feat: 28 - add private routes to proxy middleware

--- src/constants/constants.ts
+++ src/constants/constants.ts
@@ -2,6 +2,9 @@ export const ROUTES = {
   HOME: '/',
   SIGN_IN: '/sign-in',
   SIGN_UP: '/sign-up',
+  HISTORY: '/history',
 } as const;
 
 export const AUTH_ROUTES = new Set<string>([ROUTES.SIGN_IN, ROUTES.SIGN_UP]);
+
+export const PRIVATE_ROUTES = new Set<string>([ROUTES.HISTORY]);
