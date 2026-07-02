3a8797b27248cc16716018576cccca8d0a2dc008
Eryka Mileuskaya <rika.gumerova@gmail.com>
2026-07-02 21:57:45 +0300

    refactor: 10 - store refactoring

--- src/store/user-store.ts
+++ src/store/user-store.ts
@@ -1,5 +1,5 @@
 import { create } from 'zustand';
-import { persist } from 'zustand/middleware';
+import { persist, createJSONStorage } from 'zustand/middleware';
 import type { AppUser } from '@/types/auth.types';
 
 type UserStore = {
@@ -17,6 +17,8 @@ export const useUserStore = create<UserStore>()(
     }),
     {
       name: 'user-storage',
+      storage: createJSONStorage(() => sessionStorage),
+      partialize: (state) => ({ id: state.user?.id }),
     },
   ),
 );
