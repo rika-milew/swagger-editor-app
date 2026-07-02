7d896d0da170d8429a99f7f028a34e7869cf9769
michaeltavyrin <105814138+bssier@users.noreply.github.com>
2026-07-02 21:07:15 +0300

    31-feat: Editor Component (#59)
    * feat: 31 - add editor header
    
    * fix: 31 - view on main page
    
    * fix: 31 - variables file remove number after point
    
    * fix: 31 - ci prettier error
    
    * fix: 31 - changes after code review
    
    * fix: 31 - code rabbit comment
    
    * fix: 31 - adaptive layout

--- src/components/swagger/editor-header/editor-header.tsx
+++ src/components/swagger/editor-header/editor-header.tsx
@@ -4,5 +4,44 @@ import classNames from 'classnames/bind';
 const cx = classNames.bind(styles);
 
 export const EditorHeader = () => {
-  return <header className={cx('editor-header')}></header>;
+  return (
+    <div className={cx('editor-header')}>
+      <div className={cx('points-container')}>
+        <div className={cx('point')}></div>
+        <div className={cx('point')}></div>
+        <div className={cx('point')}></div>
+      </div>
+
+      <div className={cx('controls-container')}>
+        <p className={cx('valid-openapi')}>● Valid OpenAPI 3.0</p>
+
+        <div className={cx('lang-switcher')}>
+          <input
+            type="radio"
+            id="yaml"
+            name="lang"
+            className={cx('radio-input')}
+            defaultChecked
+          />
+          <label htmlFor="yaml" className={cx('radio-label')}>
+            YAML
+          </label>
+
+          <input
+            type="radio"
+            id="json"
+            name="lang"
+            className={cx('radio-input')}
+          />
+          <label htmlFor="json" className={cx('radio-label')}>
+            JSON
+          </label>
+        </div>
+
+        <button type="button" className={cx('save-code')}>
+          Save
+        </button>
+      </div>
+    </div>
+  );
 };
