c6be42ac468a42fa4bd34e39a16c5fd16f5ff686
Нина Евлаш <evlash.nina@mail.ru>
2026-07-02 12:35:02 +0200

    refactor: 11 - move language switcher to separate component and add tests

--- src/components/layout/header/header.tsx
+++ src/components/layout/header/header.tsx
@@ -1,29 +1,15 @@
 'use client';
 
-import { useRouter, usePathname } from 'next/navigation';
-import { useLocale } from 'next-intl';
 import { useTranslations } from 'next-intl';
-import { Box, Button, Flex, HStack, Separator, Text } from '@chakra-ui/react';
-import { colors } from '@/theme/colors';
+import { Box, Button, Flex, HStack } from '@chakra-ui/react';
 import { buttons } from '@/theme/buttons';
 import { container } from '@/theme/container';
 import Navigation from './navigation';
 import Logo from '../logo/logo';
-import type { Locale } from '@/i18n/routing';
+import LanguageSwitcher from './language-switcher';
 
 export default function Header() {
-  const router = useRouter();
-  const pathname = usePathname();
-  const locale = useLocale();
   const t = useTranslations('Buttons');
-
-  const switchLanguage = () => {
-    const newLocale: Locale = locale === 'en' ? 'ru' : 'en';
-
-    const cleanPath = pathname.replace(/^\/(en|ru)/, '');
-
-    router.replace(`/${newLocale}${cleanPath}`);
-  };
 
   return (
     <Box as="header" {...container.headerBox}>
@@ -34,35 +20,7 @@ export default function Header() {
         </HStack>
 
         <HStack gap="4">
-          <Button
-            onClick={switchLanguage}
-            size="sm"
-            {...buttons.languageSwitcher}
-          >
-            <Text
-              as="span"
-              color={
-                locale === 'en' ? colors.brandPrimary : colors.colorZinc600
-              }
-            >
-              {t('languageOne')}
-            </Text>
-            {' / '}
-            <Text
-              as="span"
-              color={
-                locale === 'ru' ? colors.brandPrimary : colors.colorZinc600
-              }
-            >
-              {t('languageTwo')}
-            </Text>
-          </Button>
-
-          <Separator
-            orientation="vertical"
-            height="20px"
-            borderColor={colors.border}
-          />
+          <LanguageSwitcher />
 
           <Button size="sm" {...buttons.signIn}>
             {t('login')}
---
+++ src/components/layout/header/language-switcher.tsx
@@ -0,0 +1,47 @@
+'use client';
+
+import { Button, Text } from '@chakra-ui/react';
+import { useLocale, useTranslations } from 'next-intl';
+import { usePathname, useRouter } from 'next/navigation';
+
+import type { Locale } from '@/i18n/routing';
+import { buttons } from '@/theme/buttons';
+import { colors } from '@/theme/colors';
+
+export default function LanguageSwitcher() {
+  const router = useRouter();
+  const pathname = usePathname();
+  const locale = useLocale();
+  const t = useTranslations('Buttons');
+
+  const handleLanguageChange = () => {
+    const newLocale: Locale = locale === 'en' ? 'ru' : 'en';
+
+    const cleanPath = pathname.replace(/^\/(en|ru)/, '');
+
+    router.replace(`/${newLocale}${cleanPath}`);
+  };
+
+  return (
+    <Button
+      aria-label="language-switcher"
+      onClick={handleLanguageChange}
+      size="sm"
+      {...buttons.languageSwitcher}
+    >
+      <Text
+        as="span"
+        color={locale === 'en' ? colors.brandPrimary : colors.colorZinc600}
+      >
+        {t('languageOne')}
+      </Text>
+      {' / '}
+      <Text
+        as="span"
+        color={locale === 'ru' ? colors.brandPrimary : colors.colorZinc600}
+      >
+        {t('languageTwo')}
+      </Text>
+    </Button>
+  );
+}
