8ef5bc7e388fccccb70698f5c4707e14a7f2a7aa
Нина Евлаш <evlash.nina@mail.ru>
2026-07-12 16:51:51 +0200

    fix: 93 - fix style swagger viewer

--- src/components/swagger-viewer/swagger-viewer.tsx
+++ src/components/swagger-viewer/swagger-viewer.tsx
@@ -3,15 +3,18 @@
 import { Box, Text, Heading, Flex } from '@chakra-ui/react';
 import { useTranslations } from 'next-intl';
 import { useMemo } from 'react';
+
 import { useSchemaStore } from '@/store/schema-store';
 import { parseSchema } from '@/utils/parse-schema';
 import { parseSwagger } from '@/utils/parse-swagger';
 import { colors } from '@/theme/colors';
 import { swagger } from '@/theme/swagger';
+
 import { EndpointList } from './endpoint-list';
 
 export function SwaggerViewer() {
   const t = useTranslations('SwaggerViewer');
+
   const detailsTranslations = {
     parameters: t('parameters'),
     requestBody: t('requestBody'),
@@ -26,6 +29,7 @@ export function SwaggerViewer() {
     addRequestBody: t('addRequestBody'),
     enterRequestBody: t('enterRequestBody'),
   };
+
   const code = useSchemaStore((state) => state.code);
   const format = useSchemaStore((state) => state.format);
 
@@ -46,15 +50,33 @@ export function SwaggerViewer() {
   }, [schema]);
 
   if (!code) {
-    return null;
+    return (
+      <Box {...swagger.swaggerContainer}>
+        <Flex flex={1} align="center" justify="center">
+          <Text color={colors.mutedForeground}>{t('loadSchema')}</Text>
+        </Flex>
+      </Box>
+    );
   }
 
   if (!schema) {
-    return <Text color={colors.destructive}>{t('invalidSchema')}</Text>;
+    return (
+      <Box {...swagger.swaggerContainer}>
+        <Flex flex={1} align="center" justify="center">
+          <Text color={colors.destructive}>{t('invalidSchema')}</Text>
+        </Flex>
+      </Box>
+    );
   }
 
-  if (!endpoints.length) {
-    return <Text>{t('noEndpoints')}</Text>;
+  if (endpoints.length === 0) {
+    return (
+      <Box {...swagger.swaggerContainer}>
+        <Flex flex={1} align="center" justify="center">
+          <Text color={colors.mutedForeground}>{t('noEndpoints')}</Text>
+        </Flex>
+      </Box>
+    );
   }
 
   return (
@@ -76,6 +98,7 @@ export function SwaggerViewer() {
 
         <Text {...swagger.topText}>{t('endpoints')}</Text>
       </Box>
+
       <EndpointList endpoints={endpoints} translations={detailsTranslations} />
     </Box>
   );
