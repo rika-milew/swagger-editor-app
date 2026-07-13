97db174b17a4d7f3d9b96fda54c7f5b0860c1c23
Eryka Mileuskaya <rika.gumerova@gmail.com>
2026-07-13 22:49:26 +0300

    feat: 96 - update test

--- src/utils/history-helpers.test.ts
+++ src/utils/history-helpers.test.ts
@@ -161,7 +161,7 @@ describe('formatLogs', () => {
     });
     const result = formatLogs([mockLog]);
 
-    const expectedStatusCode = 0;
+    const expectedStatusCode = HTTP_STATUS.BAD_GATEWAY;
     const expectedDuration = '0ms';
     const expectedSize = '0 B';
 
